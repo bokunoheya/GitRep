50e5565f080befe626b8a1ecb1cdfc2122761599
bokunoheya <lelong97@mail.ru>
2020-02-17 10:31:10 +0900

    commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+    startActivity(intent);
+}
+}
